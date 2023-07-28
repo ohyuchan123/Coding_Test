ab398357e78284128da89fc10e1cdbb0f828ee6e
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-07-29 03:23:55 +0900

    :truck: 파일명 변경

--- day4/avg.py
+++
@@ -1,9 +0,0 @@
-#정수 배열 numbers가 매개변수로 주어집니다. 
-#numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
-
-a = [1,2,3,4,5,6,7,8,9,10]
-
-print(sum(a)/len(a))
-
-def solution(numbers):
-    return sum(numbers)/len(numbers)
--- day5/Flip Array.py
+++
@@ -1,9 +0,0 @@
-#정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
-# num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
-
-num = [1,2,3,4,5]
-num= num[::-1]
-print(num)
-
-def solution(num_list):
-    return num_list[::-1]
